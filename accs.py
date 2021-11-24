15b7272d90ca44d973662d483134fa08d0a2e6e2
ostapfamily <shake90@mail.ru>
2021-11-24 14:54:16 +0500

    24 нояб

--- accs.py
+++ accs.py
@@ -5,10 +5,10 @@ my_app_secret = 'my_app_secret'
 
 # def get_new_token():
 #     # r = requests.get(
-#     #     "https://graph.facebook.com/oauth/access_token?client_id="+my_app_id+"&client_secret="+my_app_secret+"&grant_type=fb_exchange_token&fb_exchange_token=EAAwDjsTCk8kBAFKa464KR8NMb4ooSDie7BZCnHS6iaRvehTk8LkmES3YZApFOxYN0Ev4C1PmxZBOaaF428sBmEbY2gJ4cA9at1Q6fz7XTeCjhektKGbSZC0r25Gz8QZC0h7ScUo0FknDMtLz6rAP4JCIayBlbZAjpVkn2knK5xCaYYZBELLMZBHqr2mwmLX7Hj0ZD"
+#     #     "https://graph.facebook.com/oauth/access_token?client_id="+my_app_id+"&client_secret="+my_app_secret+"&grant_type=fb_exchange_token&fb_exchange_token=my_access_token"
 #     # )
 #     # r.json()
-#     r = "https://graph.facebook.com/oauth/access_token?client_id="+my_app_id+"&client_secret="+my_app_secret+"&grant_type=fb_exchange_token&fb_exchange_token=EAAwDjsTCk8kBADZCDCSE3U9S18ydDnuTr20zE7s3B5GcUvCcEt1h9z9sRokdwxx60pCGaKc52JriQ3eWvSIeefBDLUITwD2FmiRwQ0ZAJcgfGLdQuMe9pwy7ivIZBvpJS2yZChZCIEPvtwCTH7V6XzyMMvFR5Uv1riLdhTX0vTwAxdhWCZAPuR2FePJh6beyB35BKTLGHriTvE86Kgto2KRYwecCJZCKkrwyjYa2WjSfwZDZD"
+#     r = "https://graph.facebook.com/oauth/access_token?client_id="+my_app_id+"&client_secret="+my_app_secret+"&grant_type=fb_exchange_token&fb_exchange_token=my_access_token"
 #     return r
 #
 #
