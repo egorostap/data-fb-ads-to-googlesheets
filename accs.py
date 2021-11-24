b36d699db957e42043f700434108d92d8f421872
ostapfamily <shake90@mail.ru>
2021-11-24 14:53:38 +0500

    24 нояб

--- accs.py
+++ accs.py
@@ -1,7 +1,7 @@
 import requests
-my_access_token = 'EAAwDjsTCk8kBAFKa464KR8NMb4ooSDie7BZCnHS6iaRvehTk8LkmES3YZApFOxYN0Ev4C1PmxZBOaaF428sBmEbY2gJ4cA9at1Q6fz7XTeCjhektKGbSZC0r25Gz8QZC0h7ScUo0FknDMtLz6rAP4JCIayBlbZAjpVkn2knK5xCaYYZBELLMZBHqr2mwmLX7Hj0ZD'
-my_app_id = '3381611441853385'
-my_app_secret = 'a0d38d2d8eba2fa13ff2d7246f344dd1'
+my_access_token = 'my_access_token'
+my_app_id = 'my_app_id'
+my_app_secret = 'my_app_secret'
 
 # def get_new_token():
 #     # r = requests.get(
